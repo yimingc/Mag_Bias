9547406c084b33a685fa316926a4bc6c638b5d22
Yiming <yimingchen86@gmail.com>
2014-03-31 20:15:27 -0700

    Fixed the bug
    Bug fixed and the code works. Added home-made normrnd.m to get rid of
    the dependency of matlab stats toolbox.

--- Mag_bias.m
+++ Mag_bias.m
@@ -28,12 +28,12 @@ I6 = eye(6);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Simulation options %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-ite_num = 10;                         % MC iteration number
+ite_num = 10;                          % MC iteration number
 sim_time = 60;                         % simulation time length, in second
 sigma_m = 1;                           % std dev of the mag meas noise, in mG
 sigma_g = 5e-3;                        % std dev of the gyro meas noise, in rad/s
 sim_freq = 100;                        % the freq to generate sim data, in Hz
-ang_rate = pi/4;                       % magnitude of the generated angular rate
+ang_rate = pi/360;                       % magnitude of the generated angular rate
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%% Simulation process %%%%
@@ -58,7 +58,7 @@ for ind_sim = 1:ite_num
     for ind_data = 1:data_leng
         euler = euler + omega(ind_data,:)'*dt;
         data(ind_data, ind_euler) = limit_pi(euler);% add euler angle
-        R_g2b = euler2R_g2b(limit_pi(euler));                              % get rotation matrix from body to global
+        R_g2b = euler2R_g2b(limit_pi(euler));                              % get rotation matrix from global to body
         data(ind_data, ind_mag) = R_g2b*x_0 + bias;             % add mag meas
     end
     data(:, ind_mag) = data(:, ind_mag) + normrnd(0, sigma_m, data_leng, 3); % add noise to mag meas
@@ -76,16 +76,16 @@ for ind_sim = 1:ite_num
     % Estimation
     z_hat_0 = [euler2R_g2b(data(1, ind_euler))*x_0+rand(3,1)*10;bias+rand(3,1)*10];% zeros(6,1);               
     z_hat = z_hat_0;                    % init estimate z_hat = [0,...,0]
-    Q = blkdiag(I3*sigma_g,I3*sigma_g*0.001);                     
+    Q = blkdiag(I3*sigma_g,I3*sigma_g);                     
     R = I3*sigma_m;
     P_0 = I6*1;
     P_plus = P_0;
     H = [I3, I3];                       % measurement
     for ind_step = 1:data_leng
-        omega = data(ind_step, ind_gyro);
+        omega_meas = data(ind_step, ind_gyro);
         y     = data(ind_step, ind_mag);
         % compute discrete system dynamics
-        [Phi, Qd] = compute_system(omega, Q, dt);
+        [Phi, Qd] = compute_system(omega_meas, Q, dt);
         % update
         z_hat_minus = Phi*z_hat;        
         P_minus = Phi*P_plus*Phi' + Qd;
